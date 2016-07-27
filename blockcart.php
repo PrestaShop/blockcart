5f0bbbb8ee0f538891a359979aa4f2a8549103bb
Maxime Biloé <maxime.biloe@gmail.com>
2016-07-27 16:56:31 +0200

    // not compatible with PrestaShop 1.7

--- blockcart.php
+++ blockcart.php
@@ -42,7 +42,7 @@ class BlockCart extends Module
 
 		$this->displayName = $this->l('Cart block');
 		$this->description = $this->l('Adds a block containing the customer\'s shopping cart.');
-		$this->ps_versions_compliancy = array('min' => '1.6', 'max' => _PS_VERSION_);
+		$this->ps_versions_compliancy = array('min' => '1.6', 'max' => '1.6.99.99');
 	}
 
 	public function assignContentVars($params)
